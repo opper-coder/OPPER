ac2f1efe0e1266aff618ad53d03932ae25d20863
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-06-30 08:07:47 +0800

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -25,28 +25,136 @@ III. KEPERLUAN DASAR
 perbaiki
 ===============================================================================================================================
 
+====================================================
+
 home
-card user admin
-user registration
-kota administrasi
-active
-login
-shopping
-	ceckout
-	pending order
-	shipping
-	success
-	complain
-	retur
-	lost/hilang
-	rusak
-	palsu
-	user menolak
-	penjual curang
-	
-transaction
+	card user admin
+	user registration
+	kota administrasi
+	active
+	login
 
 finance
+	circle
+		pemasukan
+		pengeluaran
+		pendapatan kota
+		pendapatan 
+	pembayaran
+		gaji
+		sewa
+		biaya
+
+	pemasukan
+		revenue
+
+chat
+	- grup fjb
+	- blokir
+	- inbox
+	- hapus
+	- kirim
+	- edit
+	- simpan kontak
+	- profil
+	- balas
+	- teruskan
+	- product chat
+	- service chat
+	- complain
+	- banned
+	- dll ?????
+
+marketplace
+
+splash
+
+kurir
+
+transaction
+	shopping
+		ceckout
+		pending order
+		shipping
+		success
+		complain
+		retur
+		lost/hilang
+		rusak
+		palsu
+		user menolak
+		penjual curang
+	splash
+		jual
+		beli
+	ojek
+		barang
+		penumpang
+	mobil penumpang
+		barang 
+		penumpang
+
+product
+	- tambah barang
+	- kategori
+	- kadaluarsa
+	- terjual
+	- tambah stock
+	- fifo lifo
+	- 
+
+policy
+	keuangan
+		- dividen
+		- gaji karyawan
+		- harga product
+		- harga layanan
+		- bayarkan 
+		- banned
+		- neraca
+		- buku besar
+		- 
+	promo
+		- discount
+		- iklan
+		- pembiayaan
+		- 
+	merchand
+		- hak
+		- kewajiban
+		- 
+		- 
+	pengelola
+		- hak
+		- kewajiban
+	
+akun management
+	- user
+	- pengelola
+	- admin
+	- 
+
+personalia
+	- daftar anggota
+	- 
+
+calender
+	
+projection
+	daftar project (tambah hapus edit)
+		no
+		nama
+		start
+		end
+		status
+		assignee
+	action guide(tool)
+	timeschedule
+	status
+	todo list
+	
+
+
 
 
 
