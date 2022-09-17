359798f01c44fbf36fd4f35618c61ade9e1eb82f
opper-coder <80605570+opper-coder@users.noreply.github.com>
2022-09-18 06:13:37 +0800

    Update i1-marketting.js

--- i1-Inspirasi/i1-marketting.js
+++ i1-Inspirasi/i1-marketting.js
@@ -1,4 +1,13 @@
-/*
--------------------------------------------------------------------------------------------------
+/* MARKETING
+-------------------------------------------------------------------------------------------
+- promo gratis ongkir di bulan promo 
+  - hitung gaji karyawan 
+  - hitung biaya operasional bensin dan makan
+  - biaya gudang dan kemasan
+  - hitung rentang 
+  - potensi kota 
+  
 
-*/ 
+
+
+*/
