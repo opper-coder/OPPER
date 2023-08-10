31ac38b1cb53c1d3f3ae7ec3ee02aeaba19274dc
opper-coder <80605570+opper-coder@users.noreply.github.com>
2023-08-10 14:51:12 +0800

    Update catatan inspirasi.js

--- catatan inspirasi.js
+++ catatan inspirasi.js
@@ -8,7 +8,7 @@
   - potensi kota 
   
 - cara kerjasama toko
-  - masukan produk ke platform kami agar barang anda di temukan oleh pelanggan
+  - masukan produk ke platform kami agar barang anda di temukan oleh pelanggan platform kami yg luas
   - bisa di kelola oleh team kami tapi gaji perbulan 3jt/bln 
   - update stock gaji 2 juta/bln
   - kelola packing 2 juta/bln
